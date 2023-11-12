6702f7e203bbfc71765a0c15ec696667124aecee
breno085 <br3noacioli@gmail.com>
2023-11-12 20:04:45 -0300

    updated readme

--- Program.cs
+++ Program.cs
@@ -29,8 +29,7 @@ for (int i = 0; i < maxGuitarras; i++)
 do
 {
 Console.WriteLine("Controle de Estoque - Instrumentos Musicais - Guitarras\n");
-Console.WriteLine("[1] - Novo\n[2] - Listar Produtos\n[3] - Remover Produtos\n[4] - Entrada de Estoque");
-Console.WriteLine("[5] - Saída de Estoque\n[0] - Sair");
+Console.WriteLine("[1] - Novo\n[2] - Listar Produtos\n[3] - Remover Produtos\n[4] - Entrada de Estoqu\n[5] - Saída de Estoque\n[0] - Sair");
 
 readResult = Console.ReadLine();
 
@@ -72,7 +71,7 @@ void RemoverProdutos()
 
     do
     {
-        Console.WriteLine("Informe a posição do livro a ser removido: ");
+        Console.WriteLine("Informe a posição da guitarra a ser removida: ");
         if (int.TryParse(Console.ReadLine(), out i))
         {
             validEntry = true;
@@ -139,7 +138,7 @@ void ListarProdutos()
 
     for (int i = 0; i < guitarraCount; i++)
     {
-        Console.WriteLine($"{i + 1}. Guitarra {guitarras[i, 0]} {guitarras[i, 1]} - {guitarras[i, 4]} - {guitarras[i, 5]} - {guitarras[i, 6]} (R$ {guitarras[i, 2]}) - {guitarras[i, 7]} em estoque");
+        Console.WriteLine($"{i + 1}. Guitarra {guitarras[i, 0]} {guitarras[i, 1]} - {guitarras[i, 4]} - {guitarras[i, 5]} - {guitarras[i, 6]} R$ {guitarras[i, 2]}) - {guitarras[i, 7]} em estoque");
     }
 }
 
--- README.md
+++ README.md
@@ -3,16 +3,15 @@
 Empresa de instrumentos musicais
 
 Software para controle de estoque de guitarras
-Devo considerar:
- - O Nome do Produto (Guitarra Cort X500, etc) (Posso colocar marca e modelo para novos produtos, 
- e mostrar o Nome do Produto quando for listar produtos)
- da forma do site barra music: Nome do Produto | Captadores | Ponte | Tipo de Pintura (preço) - quantidade em estoque
- - O Preço
- - Quantidade
-Devo acrescentar mais 3 atributos para o produto e quantidade inicial deve ser 0
- - Corpo (Les Paul, Strato, Super Strato, Tele, Semi Hollow, etc)
- - Captadores (HH, HSS, SSS, etc)
- - Ponte (Fixa, Flutuante, Semi Flutuante)
- - Cor e Acabamento
 
-Fazer o vídeo de 1 minuto para 3-4 guitarras
+Foi considerado para o produto:
+ - A marca
+ - O Modelo
+ - O Preço
+ - O Corpo (Les Paul, Strato, Super Strato, Tele, Semi Hollow, etc)
+ - Os Captadores (HH, HSS, SSS, etc)
+ - A Ponte (Fixa, Flutuante, Semi Flutuante)
+ - A Cor e Acabamento
+ - A Quantidade
+ - A forma de listar os produtos:
+    Indice. Nome do Produto (Guitarra + marca + modelo) | Captadores | Ponte | Tipo de Pintura | Preço - quantidade em estoque
